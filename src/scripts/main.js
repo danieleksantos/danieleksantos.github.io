75468131aec499acd8e94ca0ba9ae8c7abfcec35
Daniele Karina dos Santos <danimistron@gmail.com>
2025-02-25 11:45:52 -0300

    add hard-skills

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 function updateProfileInfo (profileData){
     const photo = document.getElementById('profile-photo')
     photo.src = profileData.photo
-    photo.alt = profileData.nome
+    photo.alt = profileData.name
 
     const name = document.getElementById('profile-name')
     name.innerText = profileData.name
@@ -20,10 +20,19 @@ function updateProfileInfo (profileData){
 }
 
 function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile-softSkills')
+    const softSkillsList = document.querySelector('.soft-skills-list')
 
-   softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+    softSkillsList.innerHTML = ''
+
+    profileData.skills.softSkills.forEach(skill => {
+        const li = document.createElement('li') 
+        li.textContent = skill 
+        li.classList.add('soft-skills-item')
+        softSkillsList.appendChild(li) 
+    })
 }
+
+
 
 (async () => {
     const profileData = await fetchProfileData()
