274c454c35fd65a51d93f253147c01a4bc83da65
Daniele Karina dos Santos <danimistron@gmail.com>
2025-02-25 05:17:50 -0300

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,25 @@
 
+function updateProfileInfo (profileData){
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.nome
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `https://wa.me/${$profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
 
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)    
+    updateProfileInfo(profileData)
 })()
